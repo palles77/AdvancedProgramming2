ecb635f2ddd43b2ebd81b577159faa9af106a805
Mikołaj Bacior <mikolajbacior@gmail.com>
2024-06-30 11:38:37 +0200

    Start

---
+++ University.Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace University.Models
+{
+    public class Student
+    {
+        public long StudentId { get; set; } = 0;
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty ;
+        public string Place_of_Birth { get; set; } = string.Empty;
+        public string Addres_line_1 { get; set; } = string.Empty;
+        public string Postal_Code {  get; set; } = string.Empty;
+        public string PESEL { get; set; } = string.Empty;
+        public DateTime? BirthDate { get; set; } = null;
+ 
+        public virtual ICollection<Subject>? Subjects { get; set; } = null;
+
+
+    }
+}
